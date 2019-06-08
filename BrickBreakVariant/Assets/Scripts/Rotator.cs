d8c96c28aaeb0ddd5613cd6d8c5d36bdfa25f9cb
Gary DiCiano <garydiciano4@gmail.com>
2019-06-08 16:59:54 -0400

    First

---
+++ BrickBreakVariant/Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+
+        if(input != 0.0f)
+        {
+            transform.Rotate(0.0f, 0.0f, -(input * rotateSpeed));
+        }
+    }
+}
