91cf2884e3a5021d674a5d60cca70ba2ccfe0d3f
Gary DiCiano <garydiciano4@gmail.com>
2019-06-08 18:32:43 -0400

    Test Scene
    Made a duplicate of my initial test scene. Ball physics and a few bricks have been created for testing purposes. As of right now I'm not sure how to create a torus-like 2D collider to contain the ball, nor how to curve the bricks so that they can align themselves around the black hole.

---
+++ BrickBreakVariant/Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Respawn();
+    }
+
+    void Respawn()
+    {
+        transform.position = Vector3.zero;
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Black Hole"))
+            Respawn();
+    }
+
+    /*
+     * 
+     * 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Stage"))
+        {
+            Vector2 currentPos = transform.position;
+            Vector2 returnDir = -(Vector2.zero - currentPos);
+            GetComponent<Rigidbody2D>().velocity = returnDir;
+        }
+    }
+    */
+}
